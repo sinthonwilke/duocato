bb9be735a6fcfe1a4ab34a42134f3c51c981cd90
Sinthon Wilke <sinthonwilke@gmail.com>
2024-05-04 17:09:20 +0700

    Initial Commit

---
+++ DuoCato/DuoCatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuoCatoApp.swift
+//  DuoCato
+//
+//  Created by Sinthon Wilke on 4/5/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct DuoCatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
