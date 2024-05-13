7d5dbb6ab1346b0e0d42b591aad42cd97638ee20
Sinthon Wilke <sinthonwilke@gmail.com>
2024-05-13 14:47:54 +0700

    update

--- DuoCato/Main.swift
+++ DuoCato/Main.swift
@@ -7,6 +7,7 @@ struct Main: View {
     @State var isUserTurn: Bool = true
     @State var isBotSpeaking: Bool = false
     @State private var messages: [Message] = [Message(text: "Hello!", isSentByUser: false)]
+    @ObservedObject var networkManager = NetworkManager()
     private var textToSpeechManager = TextToSpeechManager()
     private var meowSpeech = [
         "Hi",
@@ -24,51 +25,61 @@ struct Main: View {
     
     func handleBotResponse() {
         if (!isUserTurn) {
-            delay(seconds: 2) {
-                if let randomMeow = meowSpeech.randomElement() {
-                    let message = Message(text: randomMeow, isSentByUser: false)
-                    messages.append(message)
-                    
-                    self.isBotSpeaking = true
-                    textToSpeechManager.speak(message.text, mode: selectedMode) {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                            self.isUserTurn.toggle()
-                            self.isBotSpeaking = false
-                        }
+            if let randomMeow = meowSpeech.randomElement() {
+                let message = Message(text: randomMeow, isSentByUser: false)
+                messages.append(message)
+                
+                self.isBotSpeaking = true
+                textToSpeechManager.speak(message.text, mode: selectedMode) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        self.isUserTurn.toggle()
+                        self.isBotSpeaking = false
                     }
                 }
             }
         }
     }
     
-    func delay(seconds: Double, closure: @escaping () -> Void) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: closure)
-    }
-    
     var body: some View {
-        ZStack{
-            userTheme.getBackgroundColor
-                .ignoresSafeArea(.all)
-            VStack(spacing: 20) {
-                HStack(spacing: 20) {
-                    ModeSelect(selectedMode: $selectedMode)
-                    ThemeSwitch()
+        if !networkManager.isConnected {
+            ZStack{
+                userTheme.getBackgroundColor
+                    .ignoresSafeArea(.all)
+                VStack(spacing: 20) {
+                    HStack(spacing: 20) {
+                        ModeSelect(selectedMode: $selectedMode)
+                        ThemeSwitch()
+                    }
+                    Avatar(selectedMode: $selectedMode, isSpeaking: $isBotSpeaking)
+                    Dialog( messages: $messages, speechString: $speechString)
+                    Microphone(speechString: $speechString, isUserTurn: $isUserTurn)
                 }
-                Avatar(selectedMode: $selectedMode, isSpeaking: $isBotSpeaking)
-                Dialog( messages: $messages, speechString: $speechString)
-                Microphone(speechString: $speechString, isUserTurn: $isUserTurn)
+                .padding(.horizontal, 20)
+                .padding(.top, 10)
             }
-            .padding(.horizontal, 20)
-            .padding(.top, 10)
-        }
-        .onChange(of: speechString) {
-            if (isUserTurn) {
-                handleUserInput()
+            .onChange(of: speechString) {
+                if (isUserTurn) {
+                    handleUserInput()
+                }
             }
-        }
-        .onChange(of: isUserTurn) {
-            if (!isUserTurn) {
-                handleBotResponse()
+            .onChange(of: isUserTurn) {
+                if (!isUserTurn) {
+                    handleBotResponse()
+                }
+            }
+        } else {
+            ZStack{
+                userTheme.getBackgroundColor
+                    .ignoresSafeArea(.all)
+                VStack(spacing: 20) {
+                    Image("catoSad")
+                        .cornerRadius(10)
+                    Text("No internet connection.")
+                        .foregroundColor(userTheme.getFontColor)
+                        .bold()
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 10)
             }
         }
     }
